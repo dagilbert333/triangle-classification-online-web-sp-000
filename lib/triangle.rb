e09e79e62e4ced9be279d0b5c06217532027dbea
David Gilbert <dagilbert333@gmail.com>
2019-07-17 05:29:46 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,25 +8,16 @@ class Triangle
   end
   
   def kind
-    if (@side1 <= 0) || (@side2 <= 0) || (@side3 <= 0)
-        raise TriangleError
-    elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
-        raise TriangleError
-    else 
-      if (@side1 == @side2) && (@side2 == @side3)
-        :equilateral
-      elsif (@side1 == @side2) || (@side2 == @side3) || (@side1 == @side3)
-        :isosceles
-      elsif (@side1 != @side2) && (@side2 != @side3)
-        :scalene
-      end
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
     end
   end
   
   class TriangleError < StandardError
-    def message 
-      "you must give the method an argument of an instance of the person class!"
-    end
   end
   
 end

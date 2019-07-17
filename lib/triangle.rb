57c07531e9d211d51ee2f45b386a62a8ac4ce4ee
David Gilbert <dagilbert333@gmail.com>
2019-07-17 05:22:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -21,13 +21,13 @@ class Triangle
         puts error.message
       end
     else 
-    if (@side1 == @side2) && (@side2 == @side3)
-      :equilateral
-    elsif (@side1 == @side2) || (@side2 == @side3) || (@side1 == @side3)
-      :isosceles
-    elsif (@side1 != @side2) && (@side2 != @side3)
-      :scalene
-    else 
+      if (@side1 == @side2) && (@side2 == @side3)
+        :equilateral
+      elsif (@side1 == @side2) || (@side2 == @side3) || (@side1 == @side3)
+        :isosceles
+      elsif (@side1 != @side2) && (@side2 != @side3)
+        :scalene
+      else 
       
     end
   end

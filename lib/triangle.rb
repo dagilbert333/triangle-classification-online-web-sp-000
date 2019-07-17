3bdd9c1d7bf117d34eb5e379323a7d907ee2aa25
David Gilbert <dagilbert333@gmail.com>
2019-07-17 05:22:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,7 +20,7 @@ class Triangle
       rescue TriangleError => error
         puts error.message
       end
-      end
+    else 
     if (@side1 == @side2) && (@side2 == @side3)
       :equilateral
     elsif (@side1 == @side2) || (@side2 == @side3) || (@side1 == @side3)

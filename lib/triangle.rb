a68ea9a8e10073ac7a5cf3b325df2d59da383637
David Gilbert <dagilbert333@gmail.com>
2019-07-17 05:11:35 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -22,9 +22,12 @@ class Triangle
       end
     end
   end
+  
+  class TriangleError < StandardError
+    # triangle error code
+  end
+  
 end
 
-class TriangleError < StandardError
-    # triangle error code
-end
+
 

7b8acf7862aa4a8e7aff2c58f5371248f335cd33
David Gilbert <dagilbert333@gmail.com>
2019-07-17 04:57:04 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,8 +18,7 @@ class Triangle
     else
       if (@side1 <= 0) || (@side2 <= 0) || (@side3 <= 0)
         raise TriangleError
-      elsif 
-        (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+      elsif (@side_1 + @side_2 <= @side_3) || (@side_1 + @side_3 <= @side_2) || (@side_2 + @side_3 <= @side_1)
         raise TriangleError
       end
     end

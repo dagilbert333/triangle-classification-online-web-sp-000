3d75bbdabff0d670b0d1e4a74c74fca39a820c59
David Gilbert <dagilbert333@gmail.com>
2019-07-17 05:41:28 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,10 +18,13 @@ class Triangle
   end
   
   def validate_triangle
-    valid_triangle = [(side1 + side2 > side3), (side1 + side3 > side2), (side2 + side3 > side1)]
-   
-    [side1, side2, side3].each { |side| valid_triangle << false if side <= 0 }
-    raise TriangleError if valid_triangle.include?(false)
+    #creates an array of boolean values basedon the input values of a,b,c ; so that if any of those return false, the method will return false, if the array has a false value in it, then the raise Triangle Error runs
+    #otherwise this returns true
+    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    #below tests to see if a,b or c is zero or negative
+    #if any of them are zero or negative, then it adds a false value to the array, so that the Triangle Error will be raised
+    [a, b, c].each { |s| real_triangle << false if s <= 0 }
+    raise TriangleError if real_triangle.include?(false)
   end
   
 end

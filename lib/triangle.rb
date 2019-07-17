d44175a79ce468ed6b7811d7e8a2491adc90c16c
David Gilbert <dagilbert333@gmail.com>
2019-07-17 05:19:19 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,7 +17,7 @@ class Triangle
     else 
       if (@side1 <= 0) || (@side2 <= 0) || (@side3 <= 0)
         begin 
-        raise TriangleError
+          raise TriangleError
         rescue TriangleError => error
           puts error.message
         end
